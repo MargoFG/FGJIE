1ee8f01e139f6d74f8c3a0e32dd5745cf878f2a1
FGLove <fugenchenfg@163.com>
2016-04-28 22:28:06 +0800

    01 创建项目

---
+++ FGJIE/FGJIE/Classes/Main/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FGJIE
+//
+//  Created by chfg on 16/4/28.
+//  Copyright © 2016年 chfg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
